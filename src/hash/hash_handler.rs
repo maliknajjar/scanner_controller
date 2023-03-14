d53ae02779760501ad5b9d5c2a0fc41f0be7db6b
malik446644 <malik446644@gmail.com>
2023-03-14 05:23:48 -0400

    progress

--- src/hash/hash_handler.rs
+++ src/hash/hash_handler.rs
@@ -1,16 +1,13 @@
 use crate::scanner_handler;
 use sha2::{Sha256, Digest};
 
-pub fn fill_images_hashes(images: &mut Vec<scanner_handler::Image>) {
+pub fn generate_and_fill_images_hashes(images: &mut Vec<scanner_handler::Image>) {
     for image in images {
-        let mut hasher = Sha256::new();
-        hasher.update(&image.data);
-        let hash_string = format!("{:x}", hasher.finalize());
-        image.hash = Some(hash_string.clone());
+        image.hash = Some(generate_hash_from_bytes(&image.data));
     }
 }
 
-pub fn generate_hash_from_bytes(bytes: Vec<u8>) -> String {
+pub fn generate_hash_from_bytes(bytes: &Vec<u8>) -> String {
     let mut hasher = Sha256::new();
     hasher.update(bytes);
     let hash_string = format!("{:x}", hasher.finalize());
--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,17 @@ fn main() {
     // scan two times and get the image struct
     let mut images: Vec<scanner_handler::Image> = Vec::new();
 
-    println!("starting scanning");
-    images.push(scanner_handler::scan_image());
-    images.push(scanner_handler::scan_image());
-
+    // println!("starting scanning");
+    // images.push(scanner_handler::scan_image());
+    // images.push(scanner_handler::scan_image());
+    
     // // two simple pixels
-    // images.push(Image { data: vec![ 255, 0, 0, 0, 255, 0, 0, 0, 255, 255, 0, 0 ], height: 2, width: 2, hash: None });
-    // images.push(Image { data: vec![ 0, 255, 0, 255, 0, 0, 0, 0, 255, 0, 255, 0 ], height: 2, width: 2, hash: None });
+    println!("starting..");
+    images.push(scanner_handler::Image { data: vec![ 255, 0, 0, 0, 255, 0, 0, 0, 255, 255, 0, 0 ], height: 2, width: 2, hash: None });
+    images.push(scanner_handler::Image { data: vec![ 0, 255, 0, 255, 0, 0, 0, 0, 255, 0, 255, 0 ], height: 2, width: 2, hash: None });
 
     // hash every image data (create a function that does this)
-    hash_handler::fill_images_hashes(&mut images);
-    for image in &images {
-        println!("hash: {}", image.hash.as_ref().unwrap());
-    }
+    hash_handler::generate_and_fill_images_hashes(&mut images);
 
     // generate pdf file from the images array
     let pdf_data = pdf_handler::generate_pdf_from_images(images);
